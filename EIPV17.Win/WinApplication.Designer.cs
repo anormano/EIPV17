4e4860162c89e462b427811d80d6edc46c4d6552
Norman <Norman@C-NORMAN>
2017-08-17 04:37:17 +0700

    s

--- EIPV17.Win/WinApplication.Designer.cs
+++ EIPV17.Win/WinApplication.Designer.cs
@@ -23,119 +23,143 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
-            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
-            this.module3 = new EIPV17.Module.EIPV17Module();
-            this.module4 = new EIPV17.Module.Win.EIPV17WindowsFormsModule();
-            this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
-            this.auditTrailModule = new DevExpress.ExpressApp.AuditTrail.AuditTrailModule();
-            this.objectsModule = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
-            this.chartModule = new DevExpress.ExpressApp.Chart.ChartModule();
-            this.chartWindowsFormsModule = new DevExpress.ExpressApp.Chart.Win.ChartWindowsFormsModule();
-            this.cloneObjectModule = new DevExpress.ExpressApp.CloneObject.CloneObjectModule();
-            this.conditionalAppearanceModule = new DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule();
-            this.dashboardsModule = new DevExpress.ExpressApp.Dashboards.DashboardsModule();
-            this.dashboardsWindowsFormsModule = new DevExpress.ExpressApp.Dashboards.Win.DashboardsWindowsFormsModule();
-            this.fileAttachmentsWindowsFormsModule = new DevExpress.ExpressApp.FileAttachments.Win.FileAttachmentsWindowsFormsModule();
-            this.htmlPropertyEditorWindowsFormsModule = new DevExpress.ExpressApp.HtmlPropertyEditor.Win.HtmlPropertyEditorWindowsFormsModule();
-            this.kpiModule = new DevExpress.ExpressApp.Kpi.KpiModule();
-            this.notificationsModule = new DevExpress.ExpressApp.Notifications.NotificationsModule();
-            this.notificationsWindowsFormsModule = new DevExpress.ExpressApp.Notifications.Win.NotificationsWindowsFormsModule();
-            this.pivotChartModuleBase = new DevExpress.ExpressApp.PivotChart.PivotChartModuleBase();
-            this.pivotChartWindowsFormsModule = new DevExpress.ExpressApp.PivotChart.Win.PivotChartWindowsFormsModule();
-            this.pivotGridModule = new DevExpress.ExpressApp.PivotGrid.PivotGridModule();
-            this.pivotGridWindowsFormsModule = new DevExpress.ExpressApp.PivotGrid.Win.PivotGridWindowsFormsModule();
-            this.reportsModuleV2 = new DevExpress.ExpressApp.ReportsV2.ReportsModuleV2();
-            this.reportsWindowsFormsModuleV2 = new DevExpress.ExpressApp.ReportsV2.Win.ReportsWindowsFormsModuleV2();
-            this.schedulerModuleBase = new DevExpress.ExpressApp.Scheduler.SchedulerModuleBase();
-            this.schedulerWindowsFormsModule = new DevExpress.ExpressApp.Scheduler.Win.SchedulerWindowsFormsModule();
-            this.scriptRecorderModuleBase = new DevExpress.ExpressApp.ScriptRecorder.ScriptRecorderModuleBase();
-            this.scriptRecorderWindowsFormsModule = new DevExpress.ExpressApp.ScriptRecorder.Win.ScriptRecorderWindowsFormsModule();
-            this.stateMachineModule = new DevExpress.ExpressApp.StateMachine.StateMachineModule();
-            this.treeListEditorsModuleBase = new DevExpress.ExpressApp.TreeListEditors.TreeListEditorsModuleBase();
-            this.treeListEditorsWindowsFormsModule = new DevExpress.ExpressApp.TreeListEditors.Win.TreeListEditorsWindowsFormsModule();
-            this.validationModule = new DevExpress.ExpressApp.Validation.ValidationModule();
-            this.validationWindowsFormsModule = new DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule();
-            this.viewVariantsModule = new DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule();
-            this.workflowModule = new DevExpress.ExpressApp.Workflow.WorkflowModule();
-            this.workflowWindowsFormsModule = new DevExpress.ExpressApp.Workflow.Win.WorkflowWindowsFormsModule();
-            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
-            //
-            // auditTrailModule
-            //
-            this.auditTrailModule.AuditDataItemPersistentType = typeof(DevExpress.Persistent.BaseImpl.AuditDataItemPersistent);
-            //
-            // dashboardsModule
-            //
-            this.dashboardsModule.DashboardDataType = typeof(DevExpress.Persistent.BaseImpl.DashboardData);
-            this.dashboardsWindowsFormsModule.DesignerFormStyle = DevExpress.XtraBars.Ribbon.RibbonFormStyle.Ribbon;
-            //
-            // reportsModuleV2
-            //
-            this.reportsModuleV2.EnableInplaceReports = true;
-            this.reportsModuleV2.ReportDataType = typeof(DevExpress.Persistent.BaseImpl.ReportDataV2);
-            this.reportsModuleV2.ShowAdditionalNavigation = false;
-            this.reportsModuleV2.ReportStoreMode = DevExpress.ExpressApp.ReportsV2.ReportStoreModes.XML;
-            //
-            // stateMachineModule
-            //
-            this.stateMachineModule.StateMachineStorageType = typeof(DevExpress.ExpressApp.StateMachine.Xpo.XpoStateMachine);
-            //
-            // workflowModule
-            //
-            this.workflowModule.RunningWorkflowInstanceInfoType = typeof(DevExpress.ExpressApp.Workflow.Xpo.XpoRunningWorkflowInstanceInfo);
-            this.workflowModule.StartWorkflowRequestType = typeof(DevExpress.ExpressApp.Workflow.Xpo.XpoStartWorkflowRequest);
-            this.workflowModule.UserActivityVersionType = typeof(DevExpress.ExpressApp.Workflow.Versioning.XpoUserActivityVersion);
-            this.workflowModule.WorkflowControlCommandRequestType = typeof(DevExpress.ExpressApp.Workflow.Xpo.XpoWorkflowInstanceControlCommandRequest);
-            this.workflowModule.WorkflowDefinitionType = typeof(DevExpress.ExpressApp.Workflow.Xpo.XpoWorkflowDefinition);
-            this.workflowModule.WorkflowInstanceKeyType = typeof(DevExpress.Workflow.Xpo.XpoInstanceKey);
-            this.workflowModule.WorkflowInstanceType = typeof(DevExpress.Workflow.Xpo.XpoWorkflowInstance);
-            // 
-            // EIPV17WindowsFormsApplication
-            // 
-            this.ApplicationName = "EIPV17";
-            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
-            this.Modules.Add(this.module1);
-            this.Modules.Add(this.module2);
-            this.Modules.Add(this.module3);
-            this.Modules.Add(this.module4);
-            this.Modules.Add(this.securityModule1);
-            this.Modules.Add(this.auditTrailModule);
-            this.Modules.Add(this.objectsModule);
-            this.Modules.Add(this.chartModule);
-            this.Modules.Add(this.chartWindowsFormsModule);
-            this.Modules.Add(this.cloneObjectModule);
-            this.Modules.Add(this.conditionalAppearanceModule);
-            this.Modules.Add(this.dashboardsModule);
-            this.Modules.Add(this.dashboardsWindowsFormsModule);
-            this.Modules.Add(this.fileAttachmentsWindowsFormsModule);
-            this.Modules.Add(this.htmlPropertyEditorWindowsFormsModule);
-            this.Modules.Add(this.kpiModule);
-            this.Modules.Add(this.notificationsModule);
-            this.Modules.Add(this.notificationsWindowsFormsModule);
-            this.Modules.Add(this.pivotChartModuleBase);
-            this.Modules.Add(this.pivotChartWindowsFormsModule);
-            this.Modules.Add(this.pivotGridModule);
-            this.Modules.Add(this.pivotGridWindowsFormsModule);
-            this.Modules.Add(this.reportsModuleV2);
-            this.Modules.Add(this.reportsWindowsFormsModuleV2);
-            this.Modules.Add(this.schedulerModuleBase);
-            this.Modules.Add(this.schedulerWindowsFormsModule);
-            this.Modules.Add(this.scriptRecorderModuleBase);
-            this.Modules.Add(this.scriptRecorderWindowsFormsModule);
-            this.Modules.Add(this.stateMachineModule);
-            this.Modules.Add(this.treeListEditorsModuleBase);
-            this.Modules.Add(this.treeListEditorsWindowsFormsModule);
-            this.Modules.Add(this.validationModule);
-            this.Modules.Add(this.validationWindowsFormsModule);
-            this.Modules.Add(this.viewVariantsModule);
-            this.Modules.Add(this.workflowModule);
-            this.Modules.Add(this.workflowWindowsFormsModule);
-            this.UseOldTemplates = false;
-            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.EIPV17WindowsFormsApplication_DatabaseVersionMismatch);
-            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.EIPV17WindowsFormsApplication_CustomizeLanguagesList);
-
-            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+         this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+         this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+         this.module3 = new EIPV17.Module.EIPV17Module();
+         this.module4 = new EIPV17.Module.Win.EIPV17WindowsFormsModule();
+         this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
+         this.auditTrailModule = new DevExpress.ExpressApp.AuditTrail.AuditTrailModule();
+         this.objectsModule = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+         this.chartModule = new DevExpress.ExpressApp.Chart.ChartModule();
+         this.chartWindowsFormsModule = new DevExpress.ExpressApp.Chart.Win.ChartWindowsFormsModule();
+         this.cloneObjectModule = new DevExpress.ExpressApp.CloneObject.CloneObjectModule();
+         this.conditionalAppearanceModule = new DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule();
+         this.dashboardsModule = new DevExpress.ExpressApp.Dashboards.DashboardsModule();
+         this.dashboardsWindowsFormsModule = new DevExpress.ExpressApp.Dashboards.Win.DashboardsWindowsFormsModule();
+         this.fileAttachmentsWindowsFormsModule = new DevExpress.ExpressApp.FileAttachments.Win.FileAttachmentsWindowsFormsModule();
+         this.htmlPropertyEditorWindowsFormsModule = new DevExpress.ExpressApp.HtmlPropertyEditor.Win.HtmlPropertyEditorWindowsFormsModule();
+         this.kpiModule = new DevExpress.ExpressApp.Kpi.KpiModule();
+         this.notificationsModule = new DevExpress.ExpressApp.Notifications.NotificationsModule();
+         this.notificationsWindowsFormsModule = new DevExpress.ExpressApp.Notifications.Win.NotificationsWindowsFormsModule();
+         this.pivotChartModuleBase = new DevExpress.ExpressApp.PivotChart.PivotChartModuleBase();
+         this.pivotChartWindowsFormsModule = new DevExpress.ExpressApp.PivotChart.Win.PivotChartWindowsFormsModule();
+         this.pivotGridModule = new DevExpress.ExpressApp.PivotGrid.PivotGridModule();
+         this.pivotGridWindowsFormsModule = new DevExpress.ExpressApp.PivotGrid.Win.PivotGridWindowsFormsModule();
+         this.reportsModuleV2 = new DevExpress.ExpressApp.ReportsV2.ReportsModuleV2();
+         this.reportsWindowsFormsModuleV2 = new DevExpress.ExpressApp.ReportsV2.Win.ReportsWindowsFormsModuleV2();
+         this.schedulerModuleBase = new DevExpress.ExpressApp.Scheduler.SchedulerModuleBase();
+         this.schedulerWindowsFormsModule = new DevExpress.ExpressApp.Scheduler.Win.SchedulerWindowsFormsModule();
+         this.scriptRecorderModuleBase = new DevExpress.ExpressApp.ScriptRecorder.ScriptRecorderModuleBase();
+         this.scriptRecorderWindowsFormsModule = new DevExpress.ExpressApp.ScriptRecorder.Win.ScriptRecorderWindowsFormsModule();
+         this.stateMachineModule = new DevExpress.ExpressApp.StateMachine.StateMachineModule();
+         this.treeListEditorsModuleBase = new DevExpress.ExpressApp.TreeListEditors.TreeListEditorsModuleBase();
+         this.treeListEditorsWindowsFormsModule = new DevExpress.ExpressApp.TreeListEditors.Win.TreeListEditorsWindowsFormsModule();
+         this.validationModule = new DevExpress.ExpressApp.Validation.ValidationModule();
+         this.validationWindowsFormsModule = new DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule();
+         this.viewVariantsModule = new DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule();
+         this.workflowModule = new DevExpress.ExpressApp.Workflow.WorkflowModule();
+         this.workflowWindowsFormsModule = new DevExpress.ExpressApp.Workflow.Win.WorkflowWindowsFormsModule();
+         this.commonLibraryModule1 = new CommonLibrary.CommonLibraryModule();
+         this.demoLibraryModule1 = new DemoLibrary.DemoLibraryModule();
+         ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+         // 
+         // auditTrailModule
+         // 
+         this.auditTrailModule.AuditDataItemPersistentType = typeof(DevExpress.Persistent.BaseImpl.AuditDataItemPersistent);
+         // 
+         // dashboardsModule
+         // 
+         this.dashboardsModule.DashboardDataType = typeof(DevExpress.Persistent.BaseImpl.DashboardData);
+         // 
+         // dashboardsWindowsFormsModule
+         // 
+         this.dashboardsWindowsFormsModule.DesignerFormStyle = DevExpress.XtraBars.Ribbon.RibbonFormStyle.Ribbon;
+         // 
+         // notificationsModule
+         // 
+         this.notificationsModule.CanAccessPostponedItems = false;
+         this.notificationsModule.NotificationsRefreshInterval = System.TimeSpan.Parse("00:05:00");
+         this.notificationsModule.NotificationsStartDelay = System.TimeSpan.Parse("00:00:05");
+         this.notificationsModule.ShowDismissAllAction = false;
+         this.notificationsModule.ShowNotificationsWindow = true;
+         this.notificationsModule.ShowRefreshAction = false;
+         // 
+         // pivotChartModuleBase
+         // 
+         this.pivotChartModuleBase.DataAccessMode = DevExpress.ExpressApp.CollectionSourceDataAccessMode.Client;
+         this.pivotChartModuleBase.ShowAdditionalNavigation = false;
+         // 
+         // reportsModuleV2
+         // 
+         this.reportsModuleV2.EnableInplaceReports = true;
+         this.reportsModuleV2.ReportDataType = typeof(DevExpress.Persistent.BaseImpl.ReportDataV2);
+         this.reportsModuleV2.ReportStoreMode = DevExpress.ExpressApp.ReportsV2.ReportStoreModes.XML;
+         // 
+         // stateMachineModule
+         // 
+         this.stateMachineModule.StateMachineStorageType = typeof(DevExpress.ExpressApp.StateMachine.Xpo.XpoStateMachine);
+         // 
+         // validationModule
+         // 
+         this.validationModule.AllowValidationDetailsAccess = true;
+         this.validationModule.IgnoreWarningAndInformationRules = false;
+         // 
+         // workflowModule
+         // 
+         this.workflowModule.RunningWorkflowInstanceInfoType = typeof(DevExpress.ExpressApp.Workflow.Xpo.XpoRunningWorkflowInstanceInfo);
+         this.workflowModule.StartWorkflowRequestType = typeof(DevExpress.ExpressApp.Workflow.Xpo.XpoStartWorkflowRequest);
+         this.workflowModule.UserActivityVersionType = typeof(DevExpress.ExpressApp.Workflow.Versioning.XpoUserActivityVersion);
+         this.workflowModule.WorkflowControlCommandRequestType = typeof(DevExpress.ExpressApp.Workflow.Xpo.XpoWorkflowInstanceControlCommandRequest);
+         this.workflowModule.WorkflowDefinitionType = typeof(DevExpress.ExpressApp.Workflow.Xpo.XpoWorkflowDefinition);
+         this.workflowModule.WorkflowInstanceKeyType = typeof(DevExpress.Workflow.Xpo.XpoInstanceKey);
+         this.workflowModule.WorkflowInstanceType = typeof(DevExpress.Workflow.Xpo.XpoWorkflowInstance);
+         // 
+         // EIPV17WindowsFormsApplication
+         // 
+         this.ApplicationName = "EIPV17";
+         this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+         this.Modules.Add(this.module1);
+         this.Modules.Add(this.module2);
+         this.Modules.Add(this.auditTrailModule);
+         this.Modules.Add(this.objectsModule);
+         this.Modules.Add(this.chartModule);
+         this.Modules.Add(this.cloneObjectModule);
+         this.Modules.Add(this.conditionalAppearanceModule);
+         this.Modules.Add(this.dashboardsModule);
+         this.Modules.Add(this.validationModule);
+         this.Modules.Add(this.kpiModule);
+         this.Modules.Add(this.notificationsModule);
+         this.Modules.Add(this.pivotChartModuleBase);
+         this.Modules.Add(this.pivotGridModule);
+         this.Modules.Add(this.reportsModuleV2);
+         this.Modules.Add(this.schedulerModuleBase);
+         this.Modules.Add(this.scriptRecorderModuleBase);
+         this.Modules.Add(this.stateMachineModule);
+         this.Modules.Add(this.treeListEditorsModuleBase);
+         this.Modules.Add(this.viewVariantsModule);
+         this.Modules.Add(this.workflowModule);
+         this.Modules.Add(this.commonLibraryModule1);
+         this.Modules.Add(this.demoLibraryModule1);
+         this.Modules.Add(this.module3);
+         this.Modules.Add(this.chartWindowsFormsModule);
+         this.Modules.Add(this.dashboardsWindowsFormsModule);
+         this.Modules.Add(this.fileAttachmentsWindowsFormsModule);
+         this.Modules.Add(this.htmlPropertyEditorWindowsFormsModule);
+         this.Modules.Add(this.notificationsWindowsFormsModule);
+         this.Modules.Add(this.pivotChartWindowsFormsModule);
+         this.Modules.Add(this.pivotGridWindowsFormsModule);
+         this.Modules.Add(this.reportsWindowsFormsModuleV2);
+         this.Modules.Add(this.schedulerWindowsFormsModule);
+         this.Modules.Add(this.scriptRecorderWindowsFormsModule);
+         this.Modules.Add(this.treeListEditorsWindowsFormsModule);
+         this.Modules.Add(this.validationWindowsFormsModule);
+         this.Modules.Add(this.workflowWindowsFormsModule);
+         this.Modules.Add(this.module4);
+         this.Modules.Add(this.securityModule1);
+         this.UseOldTemplates = false;
+         this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.EIPV17WindowsFormsApplication_DatabaseVersionMismatch);
+         this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.EIPV17WindowsFormsApplication_CustomizeLanguagesList);
+         ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
 
         }
 
@@ -177,5 +201,7 @@
         private DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule viewVariantsModule;
         private DevExpress.ExpressApp.Workflow.WorkflowModule workflowModule;
         private DevExpress.ExpressApp.Workflow.Win.WorkflowWindowsFormsModule workflowWindowsFormsModule;
-    }
+      private CommonLibrary.CommonLibraryModule commonLibraryModule1;
+      private DemoLibrary.DemoLibraryModule demoLibraryModule1;
+   }
 }
