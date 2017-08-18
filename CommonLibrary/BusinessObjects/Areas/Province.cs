2a7a64d4aa75a505eeb1ffe0de2243f54ada6a8f
Norman <Norman@C-NORMAN>
2017-08-18 18:48:23 +0700

    c

--- CommonLibrary/BusinessObjects/Areas/Province.cs
+++ CommonLibrary/BusinessObjects/Areas/Province.cs
@@ -17,11 +17,12 @@ namespace CommonLibrary.BusinessObjects.Areas
    [DefaultClassOptions]
    [NavigationItem("Administration")]
    [ImageName("BO_Map")]
+   [RuleCombinationOfPropertiesIsUnique("Code,Country")]
    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
    //[Persistent("DatabaseTableName")]
    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
-   public class Province : Area
+   public class Province : XPLiteObject
    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
       public Province(Session session)
           : base(session)
@@ -32,22 +33,51 @@ namespace CommonLibrary.BusinessObjects.Areas
          base.AfterConstruction();
          // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
       }
-      //private string _PersistentProperty;
-      //[XafDisplayName("My display name"), ToolTip("My hint message")]
-      //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
-      //[Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)]
-      //public string PersistentProperty {
-      //    get { return _PersistentProperty; }
-      //    set { SetPropertyValue("PersistentProperty", ref _PersistentProperty, value); }
-      //}
+        //private string _PersistentProperty;
+        //[XafDisplayName("My display name"), ToolTip("My hint message")]
+        //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
+        //[Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)]
+        //public string PersistentProperty {
+        //    get { return _PersistentProperty; }
+        //    set { SetPropertyValue("PersistentProperty", ref _PersistentProperty, value); }
+        //}
 
-      //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
-      //public void ActionMethod() {
-      //    // Trigger a custom business logic for the current record in the UI (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112619.aspx).
-      //    this.PersistentProperty = "Paid";
-      //}
+        //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
+        //public void ActionMethod() {
+        //    // Trigger a custom business logic for the current record in the UI (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112619.aspx).
+        //    this.PersistentProperty = "Paid";
+        //}
+        int code;
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        [RuleRequiredField]
+        [Key]
+        public int Code
+        {
+            get
+            {
+                return code;
+            }
+            set
+            {
+                SetPropertyValue("Code", ref code, value);
+            }
+        }
+        string name;
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        [RuleRequiredField]
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                SetPropertyValue("Name", ref name, value);
+            }
+        }
 
-      Country country;
+        Country country;
       [Association("Country-Provinces")]
       [RuleRequiredField]
       public Country Country
