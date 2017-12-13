54cce2771b60daf2e714a0477b8a411d22e5a4cf
Norman <gue@normansyah.com>
2017-12-13 16:27:59 +0700

    V17.2

---
+++ EIPV17.Mobile/ResourcesHttpHandler.ashx
@@ -0,0 +1,1 @@
+<%@ WebHandler Language="C#" CodeBehind="ResourcesHttpHandler.ashx.cs" Class="EIPV17.Mobile.ResourcesHttpHandler" %>
---
+++ EIPV17.Mobile/ResourcesHttpHandler.ashx.cs
@@ -0,0 +1,7 @@
+using System;
+using DevExpress.ExpressApp.Mobile;
+
+namespace EIPV17.Mobile {
+    public class ResourcesHttpHandler : MobileResourcesHttpHandler<EIPV17MobileApplication> {
+    }
+}
