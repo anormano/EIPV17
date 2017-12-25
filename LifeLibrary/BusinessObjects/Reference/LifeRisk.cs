877d038cbaf304f7b78837ad40cce4da107bf356
norman <gue@normansyah.com>
2017-12-26 04:53:28 +0700

    sd

---
+++ LifeLibrary/BusinessObjects/Reference/LifeRisk.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.ExpressApp.Editors;
+
+namespace LifeLibrary.BusinessObjects.Reference
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    [DefaultProperty("Name")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class LifeRisk : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        public LifeRisk(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        [RuleRequiredField]
+        public string Name { get; set; }
+        [RuleRequiredField]
+        public string Abbreviation { get; set; }
+        [EditorAlias(EditorAliases.HtmlPropertyEditor)]
+        [Size(SizeAttribute.Unlimited)]
+        public string Description { get; set; }
+    }
+}
